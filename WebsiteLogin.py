604a3424c97f6ceee6676658e22b8c0e6264b95b
Johnny <johnnymedlock1@gmail.com>
2020-01-29 20:28:39 -0600

    Added extra steps to open SOC dashboard

--- WebsiteLogin.py
+++ WebsiteLogin.py
@@ -20,10 +20,29 @@ passwd.send_keys("^8sUuYtz7Mmap5J8")
 passwd.send_keys(Keys.RETURN)
 
 sleep(5)
-browser.get("https://52.173.17.34/p/app/#!/adom/dvm/groups/-3/")
+browser.get("https://52.173.17.34/p/app/#!/adom/soc/noc/threat-monitor")
 sleep(5)
-button = browser.find_element_by_class_name("btn-primary")
-button.click()
+dashboard_button = browser.find_element_by_link_text("TAMKO-DASHBOARD")
+dashboard_button.click()
+sleep(5)
+hide_sidebar_button = browser.find_element_by_class_name("uib-btn-radio")
+hide_sidebar_button.click()
+sleep(1)
+fullscreen_button = browser.find_element_by_class_name("fullscreen-btn")
+fullscreen_button.click()
+
+sleep(4)
+browser.close()
+
+
+
+# while True:
+#     sleep(60)
+#     browser.refresh()
+#     sleep(5)
+#     button = browser.find_element_by_class_name("btn-primary")
+#     button.click()
+
 
 # okButton = browser.find_element_by_id("submit-OK")
 # okButton.send_keys(Keys.RETURN)
